f72aa209aa4eeac4a4f7c5585a22c44eac754253
David R. Jenni <david.r.jenni@gmail.com>
2017-01-06 07:24:21 +0100

    ds: add doubly-linked list.

--- ds/lists.go
+++ ds/lists.go
@@ -71,3 +71,113 @@ func (l *SList) Dequeue() (V, bool) { return l.Pop() }
 
 // Len returns the number of elements in the list.
 func (l *SList) Len() int { return l.n }
+
+// --- DList -------
+
+// dnode represents a node
+// in a doubly-linked list.
+type dnode struct {
+	n *dnode // next pointer
+	p *dnode // previous pointer
+	v V      // value
+}
+
+// DList represents a doubly-linked list.
+type DList struct {
+	r *dnode // sentinel node, the root
+	n int    // number of elements
+}
+
+// get returns the node at the
+// given index or nil if not found.
+func (l *DList) get(i int) *dnode {
+	var n *dnode
+	if i < l.n/2 {
+		n = l.r.n
+		for j := 0; j < i; j++ {
+			n = n.n
+		}
+	} else {
+		n = l.r
+		for j := l.n; j > i; j-- {
+			n = n.p
+		}
+	}
+	return n
+}
+
+// Len returns the number of elements in the list.
+func (l *DList) Len() int { return l.n }
+
+// Add adds an element to the list at the
+// given index and reports whether it was
+// successful or not.
+//
+// This operation has a time complexity
+// of O(min{i, n-i}).
+func (l *DList) Add(i int, v V) bool {
+	if i < 0 || i > l.n {
+		return false
+	}
+	// lazy initialization
+	if l.r == nil {
+		l.r = new(dnode)
+		l.r.n = l.r
+		l.r.p = l.r
+	}
+	p := l.get(i)
+	n := &dnode{
+		v: v,
+		p: p.p,
+		n: p,
+	}
+	n.n.p = n
+	n.p.n = n
+	l.n++
+	return true
+}
+
+// Remove removes the element of the list at the
+// given index and reports whether the operation was
+// successful or not.
+//
+// This operation has a time complexity
+// of O(min{i, n-i}).
+func (l *DList) Remove(i int) (V, bool) {
+	if i < 0 || i > l.n-1 {
+		return nil, false
+	}
+	n := l.get(i)
+	n.p.n = n.n
+	n.n.p = n.p
+	n.n = nil
+	n.p = nil
+	l.n--
+	return n.v, true
+}
+
+// Get returns the element at the given index.
+//
+// This operation has a time complexity
+// of O(min{i, n-i}).
+func (l *DList) Get(i int) (V, bool) {
+	if i < 0 || i > l.n-1 {
+		return nil, false
+	}
+	return l.get(i).v, true
+}
+
+// Set sets the element at the given index
+// and returns the old one.
+//
+// This operation has a time complexity
+// of O(min{i, n-i}).
+func (l *DList) Set(i int, v V) (V, bool) {
+	if i < 0 || i > l.n-1 {
+		return nil, false
+	}
+	n := l.get(i)
+	t := n.v
+	n.v = v
+	return t, true
+}
--- ds/lists_test.go
+++ ds/lists_test.go
@@ -71,3 +71,71 @@ func TestSListQueue(t *testing.T) {
 		t.Errorf("want %d, got %d", 0, l.Len())
 	}
 }
+
+func TestDList(t *testing.T) {
+	const n = 65
+	var l DList
+
+	if _, ok := l.Get(0); ok {
+		t.Errorf("no element at index 0")
+	}
+	if _, ok := l.Get(-1); ok {
+		t.Errorf("no element at index -1")
+	}
+
+	if _, ok := l.Set(0, 1); ok {
+		t.Errorf("no element to set at index 0")
+	}
+	if _, ok := l.Set(-1, 1); ok {
+		t.Errorf("no element to set at index -1")
+	}
+
+	if _, ok := l.Remove(0); ok {
+		t.Errorf("no element to remove at index 0")
+	}
+	if _, ok := l.Remove(-1); ok {
+		t.Errorf("no element to remove at index -1")
+	}
+
+	for i := 0; i < n; i++ {
+		if ok := l.Add(i, i); !ok {
+			t.Errorf("cannot add: %d", i)
+		}
+	}
+	if l.Len() != n {
+		t.Errorf("want %d, got %d", n, l.Len())
+	}
+
+	for i := 0; i < n; i++ {
+		v, ok := l.Get(i)
+		if !ok {
+			t.Errorf("not found: %d", i)
+			continue
+		}
+		l.Set(i, v.(int)*v.(int))
+	}
+
+	for i := n - 1; i >= 0; i -= 2 {
+		r, ok := l.Remove(i)
+		if !ok {
+			t.Errorf("cannot remove: %d", i)
+			continue
+		}
+		if r != i*i {
+			t.Errorf("want %d, got %v", i*i, r)
+		}
+	}
+
+	if l.Len() != n/2 {
+		t.Errorf("want %d, got %d", n/2, l.Len())
+	}
+
+	for i := 0; i < n/2+1; i++ {
+		if ok := l.Add(i*2, i); !ok {
+			t.Errorf("cannot add: %d", i)
+		}
+	}
+	if l.Len() != n {
+		t.Errorf("want %d, got %d", n, l.Len())
+	}
+}
